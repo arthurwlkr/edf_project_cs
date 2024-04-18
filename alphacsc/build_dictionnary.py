bb925ea5b1cd804b903dc3273e8e0ce1143d8d1f
pcast31 <paul.casteras@student-cs.fr>
2024-04-18 18:19:54 +0200

    Commentaires alphacsc

--- alphacsc/build_dictionnary.py
+++ alphacsc/build_dictionnary.py
@@ -6,13 +6,13 @@ import datetime
 from learn_d_z import learn_d_z
 from tqdm import tqdm
 
+# Le but de ce fichier est de construire les jeux de données de signaux de références et de signaux d'activations
 
 
-
-calendrier_challenge = pd.read_parquet("challenge_data/calendrier_challenge.parquet")
-questionnaire = pd.read_parquet("challenge_data/questionnaire.parquet")
-consos_challenge = pd.read_parquet("challenge_data/consos_challenge.parquet")
-temperatures = pd.read_parquet("challenge_data/temperatures.parquet")
+calendrier_challenge = pd.read_parquet("Données/challenge_data/calendrier_challenge.parquet")
+questionnaire = pd.read_parquet("Données/challenge_data/questionnaire.parquet")
+consos_challenge = pd.read_parquet("Données/challenge_data/consos_challenge.parquet")
+temperatures = pd.read_parquet("Données/challenge_data/temperatures.parquet")
 
 
 
@@ -21,7 +21,7 @@ all_clients = list(set(consos_challenge["id_client"]))
 day = datetime.datetime(2011,1,2)-datetime.datetime(2011,1,1)
 
 
-#penalisation
+# Penalisation
 n_days = 365
 n_times = n_days*48
 n_atoms = 15
@@ -36,12 +36,15 @@ reg = reg*coef_reg
 
 
 def load_signal(id_client,min_time,max_time):
+    """
+    Load the signal of id_client between min_time and max_time
+    """
     consos = consos_challenge[consos_challenge["id_client"]==id_client]
     consos = consos[consos["horodate"]>=min_time]
     return consos[consos["horodate"]<max_time]["puissance_W"]
 
 
-
+# For each client create and save the activation signals and the reference signals
 for id_client in tqdm(all_clients):
     signal = np.array(load_signal(id_client,min_date,min_date+n_days*day))
     X = signal.reshape(1,len(signal))
@@ -49,19 +52,8 @@ for id_client in tqdm(all_clients):
     spike = cdl[3]
     atoms = cdl[2]
 
-    np.save('data_spike/'+str(id_client)+'.npy',spike)
-    np.save('data_atoms/'+str(id_client)+'.npy',atoms)
-
-
-
-
-
-
-
-
-
-
-
+    np.save('alphacsc/data_spike/'+str(id_client)+'.npy',spike)
+    np.save('alphacsc/data_atoms/'+str(id_client)+'.npy',atoms)
 
 
 
